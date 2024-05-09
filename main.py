b1fcd4b9a02d14f5426a6822b5402f123c84f94f
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-10 02:59:48 +0700

    Duplicate headers

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ async def read_user_agent(user_agent: Annotated[str | None, Header()] = None):
 @app.get("/headers/under_score")
 async def read_under_score(under_score: Annotated[str | None, Header(convert_underscores = False)] = None):
   return {"under_score": under_score}
+
+# Duplicate headers
+@app.get("/header/x_tokens")
+async def read_x_tokens(x_tokens: Annotated[list[str] | None, Header()] = None):
+  return {"x_tokens": x_tokens}
